dc8d52ccb5442dcffd76ce55a129ed892acb3d34
Pinchai <>
2023-12-06 18:01:19 +0700

    loop and function

--- String.py
+++ String.py
@@ -1,7 +1,19 @@
+id = int(input('Enter Your ID (1-100): '))
+name = str(input('Enter Your name (3-12): '))
+gender = str(input('Enter Your gender (M, F): '))
+while not(gender == 'M' or gender == 'F'):
+    gender = str(input('Please Enter Your gender again! (M, F): '))
+group = str(input('Enter Your group: '))
 
+student = []
+student.append(
+    {
+        'id': id,
+        'name': name,
+        'gender': gender,
+        'group': group,
+    },
+)
 
-data = ['a', 'b', 'c']
-for item in data:
-  if item == 'c':
-    break
-  print(item)
+for item in student:
+    print(f"Id: {item['id']} - Name: {item['name']}")
